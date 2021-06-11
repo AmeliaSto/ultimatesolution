5dfb16f154ecf0670e6dd79052b322d9583b79b8
amelia <amelia.izak@gmail.com>
2021-06-11 08:43:59 +0200

    my first commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace MyUltimateSolution
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Today is Friday!");
+            Random random = new Random();
+            for (int i = 0; i < 10; i++)
+            {
+                System.Threading.Thread.Sleep(1000);
+                Console.WriteLine(random.Next(100,999));
+            }
+        }
+    }
+}
